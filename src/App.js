0749a2b5efa582e502d01073bd41838e9eda7fc4
Sangeeta Yadav <sangeeta.yadav@joshsoftware.com>
2023-02-24 12:15:08 +0000

    camera component ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import { Camera } from './components/camera';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="jv-page d-flex flex-column mx-auto text-center py-5 px-4">
+      <Camera/>
     </div>
   );
 }
---
+++ src/components/camera.jsx
@@ -0,0 +1,22 @@
+
+export function Camera () {
+  return (
+    <>
+      <div className="page-top-section flex-1">
+        <h6 className="main-heading mb-5 text-white">What do you wanna create?</h6>
+        <div className="image-upload-box d-flex align-items-center justify-content-center border mb-5 mx-auto">
+        </div>
+        <button className="camera-btn d-block mb-4 mx-auto border-0"></button>
+      </div>
+      <div className="camera-page-btns">
+        <label className="custom-file-upload mb-4 fw-bold">
+          <input type="file" />
+          Upload a photo
+        </label>
+        <button class="btn btn-default text-uppercase d-block m-auto">
+          Next
+        </button>
+      </div>
+    </>
+  );
+}
