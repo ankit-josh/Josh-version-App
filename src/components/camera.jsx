c1697e525a5bb0b68578987471691272c3cacb25
Sangeeta Yadav <sangeeta.yadav@joshsoftware.com>
2023-02-24 12:59:11 +0000

    modified changes

--- src/components/camera.jsx
+++ src/components/camera.jsx
@@ -5,7 +5,7 @@ export function Camera () {
       <div className="page-top-section flex-1">
         <div className="image-upload-box d-flex align-items-center justify-content-center border mb-5 mx-auto">
         </div>
-        <button className="camera-btn d-block mb-4 mx-auto border-0"></button>
+        <button className="btn-camera d-block mb-4 mx-auto border-0"></button>
       </div>
       <div className="camera-page-btns">
         <label className="custom-file-upload mb-4 fw-bold">
